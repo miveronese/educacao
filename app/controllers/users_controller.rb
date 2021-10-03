37c3e432f7e72838fa2593ddd3da012929caae47
michelle <mverone@gmail.com>
2021-10-02 23:30:20 -0700

    Fix creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+    
     def create
       @user = User.new(user_params)
       if @user.save
         redirect_to root_url, notice: "Email enviado com sucesso!"
+      else
+        redirect_to root_url, notice: @user.errors.full_messages[0]
+      
       end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-    # validates :email,
-    # format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
-    #         uniqueness: { case_sensitive: false },
-    #         length: { minimum: 4, maximum: 254 }
+    validates :email, uniqueness: { case_sensitive: false, message: "já cadastrado" }
 end
+
+# "Hey #{object.name}, #{data[:value]} is already taken."
